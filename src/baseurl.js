aad24f59de7ad757bf5048148a9a3282ff2663ef
DawrAli33285 <lemightyeagle@gmail.com>
2026-09-18 07:32:40 -0700

    new

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,4 +1,4 @@
 // export const BASE_URL="http://localhost:5000/api"
 
-export const BASE_URL="https://newretntionbackend.vercel.app"
+export const BASE_URL="https://retentionbackend-one.vercel.app"
 // export const BASE_URL="https://employeengagements.com/api"
